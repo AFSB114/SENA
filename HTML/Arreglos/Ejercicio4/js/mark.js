49a5b017fce98ddb4b52a4ecc6cd8ae60d5680fa
AFSB114 <asuazab13@gmail.com>
2024-07-21 20:04:12 -0500

    Add New Functions

--- HTML/Arreglos/Ejercicio4/js/mark.js
+++ HTML/Arreglos/Ejercicio4/js/mark.js
@@ -1,20 +1,50 @@
 function mark() {
+
     setTimeout(() => {
         let table = document.getElementById("table-1")
+
+        let letters = ['B', 'I', 'N', 'G', 'O']
+
+        document.addEventListener('keydown', (event) => {
+            if (event.key.toLowerCase() === 'x') {
+                for (let index1 = 0; index1 < 5; index1++) {
+                    for (let index2 = 0; index2 < 5; index2++) {
+                        if (index1 === index2 || index1 + index2 === 4) {
+                            table.children[1].children[index1].children[index2].children[0].children[1].style.display = 'block'
+                            table.children[1].children[index1].children[index2].children[0].children[1].value = true
+                        }
+                    }
+                }
+            }
+
+            for (let index1 = 0; index1 < 5; index1++) {
+                if (event.key.toUpperCase() === letters[index1]) {
+                    for (let index2 = 0; index2 < 5; index2++) {
+                        table.children[1].children[index2].children[index1].children[0].children[1].style.display = 'block'
+                        table.children[1].children[index2].children[index1].children[0].children[1].value = true
+                    }
+                }
+            }
+
+            if (event.key.toLowerCase() === 'c') {
+                for (let index1 = 0; index1 < 5; index1++) {
+                    for (let index2 = 0; index2 < 5; index2++) {
+                        table.children[1].children[index1].children[index2].children[0].children[1].style.display = 'none'
+                        table.children[1].children[index1].children[index2].children[0].children[1].value = false
+                    }
+                }
+            }
+        })
 
         for (let index1 = 0; index1 < 5; index1++) {
             for (let index2 = 0; index2 < 5; index2++) {
                 let circle = createCircle()
 
                 circle.classList.add('mark', 'bg-danger-subtle')
-                
-                if (index1 === 2 && index2 === 2) {
-                    circle.style.display = 'block'
-                    circle.value = true
-                } else {
-                    circle.style.display = 'none'
-                    circle.value = false
-                }
+
+                circle.style.display = 'none'
+                circle.value = false
+
 
                 table.children[1].children[index1].children[index2].children[0].appendChild(circle)
 
@@ -27,10 +57,15 @@ function mark() {
                         table.children[1].children[index1].children[index2].children[0].children[1].value = false
                     }
                 })
+
+                table.children[0].children[0].children[index1].children[0].addEventListener('click', () => {
+                    table.children[1].children[index2].children[index1].children[0].children[1].style.display = 'block'
+                    table.children[1].children[index2].children[index1].children[0].children[1].value = true
+                })
             }
         }
 
-    }, 3);
+    }, 3)
 }
 
 window.addEventListener('DOMContentLoaded', mark())

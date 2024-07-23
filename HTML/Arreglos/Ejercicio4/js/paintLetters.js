0c0b2146a0560caac3c47c508c74982a28dd4618
AFSB114 <asuazab13@gmail.com>
2024-07-23 09:54:24 -0500

    New Commit

--- HTML/Arreglos/Ejercicio4/js/paintLetters.js
+++ HTML/Arreglos/Ejercicio4/js/paintLetters.js
@@ -3,13 +3,13 @@ function paintLetters() {
         for (let index = 4; index < 9; index++) {
 
             let table = document.getElementById(`table-${index}`)
-    
+
             let letter = index - 4
-    
+
             for (let index1 = 0; index1 < 5; index1++) {
 
                 let circle = createCircle()
-    
+
                 let cell = table.children[1].children[index1].children[letter].children[0]
 
                 let color = ''
@@ -33,13 +33,13 @@ function paintLetters() {
                         color = 'secondary'
                         break
                 }
-                
-                circle.classList.add('mark',`bg-${color}-subtle`)
+
+                circle.classList.add('mark', `bg-${color}-subtle`)
                 cell.appendChild(circle)
-    
+
             }
         }
-    },2)
+    }, 2)
 }
 
 window.addEventListener('DOMContentLoaded', paintLetters())
